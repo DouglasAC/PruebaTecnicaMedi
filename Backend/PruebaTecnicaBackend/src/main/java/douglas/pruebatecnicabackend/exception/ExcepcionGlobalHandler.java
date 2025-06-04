5ec1dd541ac13af2d5e18c5fd522278ff64b1dbd
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2025-06-04 10:28:07 -0600

    Add global exception handler for employee-related exceptions

---
+++ Backend/PruebaTecnicaBackend/src/main/java/douglas/pruebatecnicabackend/exception/ExcepcionGlobalHandler.java
@@ -0,0 +1,33 @@
+package douglas.pruebatecnicabackend.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ExcepcionGlobalHandler {
+
+    @ExceptionHandler(EmpleadoNoEncontradoException.class)
+    public ResponseEntity<Object> manejarEmpleadoNoEncontradoException(EmpleadoNoEncontradoException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("mensaje", ex.getMessage());
+        body.put("timestamp", LocalDateTime.now());
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NumeroIdentificacionYaExisteException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> manejarDatoDuplicado(NumeroIdentificacionYaExisteException ex) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("mensaje", ex.getMessage());
+        return body;
+    }
+}
