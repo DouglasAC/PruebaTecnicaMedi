1a043ccfa1ffd05e810ba6db565cfda896cef511
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2025-06-04 12:45:28 -0600

    Add navigation bar with authentication links and logout functionality

--- Frontend/prueba-tecnica-frontend/src/app/app.component.ts
+++ Frontend/prueba-tecnica-frontend/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'prueba-tecnica-frontend';
+
+ constructor(
+    public authService: AuthService,
+    private router: Router
+  ) {}
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
